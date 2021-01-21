06d78bb2bfa8ef2a8bdf2554d8ffe76fde7cb15b
Eric Sahm <eric.sahm@estech-solutions.com>
2021-01-21 09:42:24 -0700

    html and css project

---
+++ JSWebPage/main.js
@@ -0,0 +1,18 @@
+// This will log to the console in Chrome
+console.log("Success");
+
+// Variables
+let color = 'red';
+
+const pi = 3.14;
+
+let fname = 'Roxy'; // String Literal
+let age = 10; //Number Literal
+let lovesCates = true; //Boolean Literal
+let color = null;
+let list; //Undefined
+
+let message = fname + ' ' + age;
+
+console.log(message)
+
